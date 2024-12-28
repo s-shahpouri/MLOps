9a488af984164339d6c58def352fffacfe2928e8
s-shahpouri <z.shahpouri@gmail.com>
2024-12-28 14:15:26 +0100

    azure.cognitiveservices.speech

--- sys/jformat.py
+++ sys/jformat.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import json
+import argparse
 
 def formatter(string, sort_keys=True, indent=4):
 
@@ -9,10 +10,22 @@ def formatter(string, sort_keys=True, indent=4):
     return json.dumps(loaded_json,sort_keys=sort_keys, indent=indent)
 
 
-def main(path):
+def main(path, no_sort):
+    if no_sort:
+        sort_keys = False
+    else:
+        sort_keys = True
+
     with open(path, 'r') as _f:
-        print(formatter(_f.read()))
+        print(formatter(_f.read()), sort_keys = sort_keys)
 
 
 if __name__ == "__main__":
-    main(sys.argv[-1])
+    parser = argparse.ArgumentParser(description = "This is the jformat tool")
+    parser.add_argument("--sort", action = 'store_true', help = 'set the sorting')
+    args = parser.parse_args()
+    print(args.sort)
+    #main(sys.argv[-1], no_sort = False)
+
+
+# To run in command line: python jformat.py -h
